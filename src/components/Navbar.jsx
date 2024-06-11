306947baf96a2156fdbd098f91610ea20d08efeb
bishtdev <bishtdev030@gmail.com>
2024-06-11 09:45:00 +0530

    updating logos

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import image2 from '../../images/logos_firebase.jpg'
 
 const Navbar = () => {
   return (
     <div className="h-[60px] bg-white rounded-lg my-4 flex items-center justify-center gap-2 text-xl">
-        <img src="./public/images/logos_firebase.jpg" alt="" />
+        <img src="../../images/logos_firebase.jpg" alt="" />
         <h1>Firebase Contact App</h1>
       
     </div>
