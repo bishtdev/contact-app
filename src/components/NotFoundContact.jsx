9864f0cd3d30b7a555c8a7c2f34388e04837e282
bishtdev <bishtdev030@gmail.com>
2024-06-11 09:59:23 +0530

    updated some image paths

--- src/components/NotFoundContact.jsx
+++ src/components/NotFoundContact.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import image1 from ''
+
 
 const NotFoundContact = () => {
   return (
     <div className="flex justify-center items-center gap-4 h-[80vh] text-white ">
       {" "}
-      <img className="w-10" src="../../images/Google_Contacts_logo.png" alt="" /> NO CONTACT
+      <img className="w-10" src="./images/Google_Contacts_logo.png" alt="" /> NO CONTACT
       FOUND
     </div>
   );
